8b0a1b42a9a7953145a9a5e95cc16f63d673ae00
Dmitry <dimatopproog2@gmail.com>
2024-09-15 15:43:51 +0300

    first commit

---
+++ src/main/java/com/project/videoProjDemo/VideoProjDemoApplication.java
@@ -0,0 +1,13 @@
+package com.project.videoProjDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoProjDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoProjDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/videoProjDemo/VideoProjDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.videoProjDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProjDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
